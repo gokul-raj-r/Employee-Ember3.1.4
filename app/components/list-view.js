ff159a79417bda23f68473aa5cbb1ac76095d1c7
Gokul Raj R <gokulraj@securden.com>
2024-04-17 11:58:11 +0530

    delete operation

--- app/components/list-view.js
+++ app/components/list-view.js
@@ -1,6 +1,9 @@
 import Component from "@ember/component";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  ajax: service(),
+  router: service(),
   data: [],
 
   init() {
@@ -17,5 +20,28 @@ export default Component.extend({
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
+  },
+
+  actions: {
+    deleteRecord(id) {
+      console.log("employee id ", id);
+
+      this.get("ajax")
+        .request(`/employee/remove/${id}`, {
+          method: "POST",
+          contentType: "application/json",
+        })
+        .then((response) => {
+          // Handle successful response
+          console.log("Response:", response);
+          this.data.filter((obj) => obj.id != id);
+        })
+        .catch((error) => {
+          // Handle error
+          console.error("Error:", error);
+        });
+
+      this.router.transitionTo("index");
+    },
   },
 });
